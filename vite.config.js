8b99235b36701ffe02ddfcc88948694e2113d245
AndreasHolmAndersen <72497878+AndreasHolmAndersen@users.noreply.github.com>
2023-05-04 14:32:05 +0200

    added configuration for vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import postcss from "postcss";
-import tailwindcss from 'tailwindcss';
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+      },
+    },
+  },
   plugins: [
     svelte(), // Configure PostCSS with TailwindCSS
     postcss({
