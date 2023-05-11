d84266195884b0daf20c4c0a01eb0d7ee77636a8
AndreasHolmAndersen <72497878+AndreasHolmAndersen@users.noreply.github.com>
2023-05-11 13:17:50 +0200

    added notification for default template

--- src/components/MainPage.svelte
+++ src/components/MainPage.svelte
@@ -14,7 +14,7 @@
     },
   });
   let email = "";
-
+  let show = false;
   function handleAnchorClick(event) {
     event.preventDefault();
     const link = event.currentTarget;
@@ -29,8 +29,12 @@
   }
 
   function handleSubmitSignup(event) {
+    show = true;
     event.preventDefault();
     amplitude.track("submit-sign-up", { email });
+    setTimeout(() => {
+      email = "";
+    }, 400);
   }
 
   function handleLoginClick(event) {
@@ -827,4 +831,80 @@
       </footer>
     </div>
   </div>
+    <!-- Notifications -->
+    <div
+    aria-live="assertive"
+    class="pointer-events-none fixed inset-0 flex w-full items-end px-4 py-6 sm:items-start sm:p-6"
+  >
+    <div
+      class="flex w-full h-full flex-col items-center space-y-4 sm:items-end"
+    >
+      <!-- Notification panel, dynamically insert this into the live region when it needs to be displayed -->
+      <!-- <transition
+          enter-active-class="transform ease-out duration-300 transition"
+          enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+          enter-to-class="translate-y-0 opacity-100 sm:translate-x-0"
+          leave-active-class="transition ease-in duration-100"
+          leave-from-class="opacity-100"
+          leave-to-class="opacity-0"
+        > -->
+      {#if show}
+        <div
+          class="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5"
+        >
+          <div class="p-4">
+            <div class="flex items-start">
+              <div class="flex-shrink-0">
+                <svg
+                  class="h-6 w-6 text-green-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div class="ml-3 w-0 flex-1 pt-0.5">
+                <p class="text-sm font-medium text-gray-900">
+                  Thank you for signing up!
+                </p>
+                <p class="mt-1 text-sm text-gray-500">
+                  We will get back to you with the latest info soon!
+                </p>
+              </div>
+              <div class="ml-4 flex flex-shrink-0">
+                <button
+                  type="button"
+                  on:click={() => {
+                    show = false;
+                  }}
+                  class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  <span class="sr-only">Close</span>
+                  <svg
+                    class="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      {/if}
+      <!-- </transition> -->
+    </div>
+  </div>
+
 </main>
