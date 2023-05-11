4900d11df3ff9fc77b1451e83dd739e0c090aea2
AndreasHolmAndersen <72497878+AndreasHolmAndersen@users.noreply.github.com>
2023-05-11 14:12:09 +0200

    added content, page done for now

--- src/components/MainPage.svelte
+++ src/components/MainPage.svelte
@@ -56,7 +56,7 @@
         <div class="flex-1">
           <span class="sr-only">Your Company</span>
           <img
-            class="h-8 w-auto"
+            class="h-32 -mt-8 w-fit"
             src={content.header.logo}
             alt="logo"
           />
@@ -190,7 +190,7 @@
                         <path
                           stroke-linecap="round"
                           stroke-linejoin="round"
-                          d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
+                          d={feature.icon}
                         />
                       </svg>
                     </div>
